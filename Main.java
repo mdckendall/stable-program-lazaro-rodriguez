4cf38ef31a0d986934290b99b040e6939554220c
lazaro20 <113481833+lazaro-rodriguez@users.noreply.github.com>
2022-10-08 01:07:00 +0000

    Added the stable and horse class and modified the main class

--- Main.java
+++ Main.java
@@ -1,17 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-class Main {
-  public static void main(String[] args) {
-    //Ask the user here
-	  Stable s = new Stable();
+public class Main {
+	public static void main(String[] args) {
+		Stable s = new Stable();
+		// Ask the user here
 		System.out.println("What is the address of the stable?");
 		Scanner scanner = new Scanner(System.in);
 		s.address = scanner.nextLine();
 		do {
-			System.out.println("Do you wish to add a horse to the stable?"); 
+			System.out.println("Do you wish to add a horse to the stable?");
 			char input = scanner.next().charAt(0);
 			scanner.nextLine();
-			if(input == 'y'){
+			if (input == 'y') {
 				Horse h;
 				System.out.println("What is the name of the horse?");
 				String hn = scanner.nextLine();
@@ -19,15 +21,15 @@ class Main {
 				double w = scanner.nextDouble();
 				System.out.println("Enter true if the horse is tame or false if it is not.");
 				boolean t = scanner.nextBoolean();
-				h = new Horse(hn, w, t);
-				s.arrayList.add(h);
-			}//testing goodest 4
-			else{
+				s.horses.add(new Horse(hn, w, t));
+
+			} else {
+
 				break;
 			}
-		}while(true);
-		//Display the info here
-                //You code here!
-	  }
-  }
+		} while (true);
+		// Display the info here
+		// You code here!
+		s.Display();
+	}
 }
--- Stable.java
+++ Stable.java
@@ -1,1 +1,26 @@
-//Create the Stable class here.
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stable {
+	String address;
+	public static List<Horse> horses = new ArrayList<Horse>();
+
+	public Stable(Horse horse) {
+		horses.add(horse);
+	}
+
+	public Stable() {
+		
+	}
+
+	public void Display() {
+		// int i=0;
+		for (int i = 0; i < horses.size(); i++) {
+
+			System.out.println("Horse #" + (i + 1) + "  Name:" + horses.get(i).getName() + "  Weight: "
+					+ horses.get(i).getWeight() + "  Tame:" + horses.get(i).getTame());
+
+		}
+	}
+
+}
